b4d1bf5cf1511fa6ee56e8c985b44d9db20b9551
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2016-03-01 15:04:00 -0300

    Comments

--- Class01/Model/BaseModel.swift
+++ Class01/Model/BaseModel.swift
@@ -1,35 +1,34 @@
-////
-////  BaseModel.swift
-////  Class01
-////
-////  Created by Henrique Valcanaia on 2/29/16.
-////  Copyright © 2016 Henrique Valcanaia. All rights reserved.
-////
 //
-//import Gloss
-//import EVReflection
+//  BaseModel.swift
+//  Class01
 //
-//class BaseModel: NSObject, Decodable {
-//    
-//    required init?(json: JSON) {
-//        super.init()
-//        let mirror = Mirror(reflecting: self)
-//        for child in mirror.children {
-//            guard let property = child.label else {
-//                print("deu ruim")
-//                break
-//            }
-//            
-//            if property == "super" { continue }
-//            
-////            let value = (property <~~ json)!
-////            <self.valueForKey(property)!.dynamicType>
-//            
-////            let type = self.valueForKey(property)!.dynamicType
-////            let a:type = (property <~~ json)!
-//            
-//            self.setValue((property <~~ json)!, forKey: property)
-//        }
-//    }
+//  Created by Henrique Valcanaia on 2/29/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
 //
-//}
+
+import Gloss
+
+class BaseModel: NSObject, Decodable {
+    
+    required init?(json: JSON) {
+        super.init()
+        let mirror = Mirror(reflecting: self)
+        for child in mirror.children {
+            guard let property = child.label else {
+                print("deu ruim")
+                break
+            }
+            
+            if property == "super" { continue }
+            
+//            let value = (property <~~ json)!
+//            <self.valueForKey(property)!.dynamicType>
+            
+//            let type = self.valueForKey(property)!.dynamicType
+//            let a = (property <~~ json)! // Here de error occurs
+            
+            self.setValue((property <~~ json)!, forKey: property) // Here the error doesn't occurs
+        }
+    }
+
+}
