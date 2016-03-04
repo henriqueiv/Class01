bf545b1393ce9b5e888b6f4f924c500c9f1a25b7
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2016-03-04 15:03:03 -0300

    Novo comiti!

--- Class01UITests/LoginUITests.swift
+++ Class01UITests/LoginUITests.swift
@@ -18,7 +18,7 @@ class LoginUITests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launchEnvironment = ["isUITest":"true"]
         XCUIApplication().launch()
